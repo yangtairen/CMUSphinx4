c0af7fa63be7cf51753eb5a4d29c1499bf3513a9
Georgiana Rodica Chelu <georgiana.chelu93@gmail.com>
2014-08-12 13:12:46 +0000

    mllr decoding demo

---
+++ sphinx4-samples/src/main/java/edu/cmu/sphinx/demo/mllrdecoding/DecodingWithMllr.java
@@ -0,0 +1,49 @@
+package edu.cmu.sphinx.demo.mllrdecoding;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+import edu.cmu.sphinx.decoder.adaptation.MllrDecoding;
+import edu.cmu.sphinx.demo.transcriber.TranscriberDemo;
+import edu.cmu.sphinx.linguist.acoustic.tiedstate.Sphinx3Loader;
+
+public class DecodingWithMllr {
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		
+		Configuration configuration = new Configuration();
+		
+		configuration.setAcousticModelPath("/home/gia/Work/CMUSphinx/Task6/en-us/");
+		configuration.setDictionaryPath("/home/gia/Work/CMUSphinx/Task6/tedlium.dic");
+		configuration.setLanguageModelPath("/home/gia/Work/CMUSphinx/Task6/cmusphinx-5.0-en-us.lm.dmp");
+		
+		StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
+		InputStream stream = TranscriberDemo.class.getResourceAsStream(
+                "/edu/cmu/sphinx/demo/decodingMllr/JamesCameron_2010_310.25_329.97.wav");
+
+		Sphinx3Loader loader = (Sphinx3Loader) recognizer.getLoader();
+		System.out.println(loader);
+		
+		String filePath = "/home/gia/Work/CMUSphinx/Task1/mllr_setup/data/JamesCameron_2010/mllr_matrix";
+		MllrDecoding test = new MllrDecoding(loader, filePath);
+		test.decodeWithMllr();
+		
+		loader = test.getNewLoader();
+		recognizer.startRecognition(stream, false);
+		
+		SpeechResult result;
+
+		while ((result = recognizer.getResult()) != null) {
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+        }
+	
+		if(result == null)
+			System.out.println("sad recognizer");
+		
+		recognizer.stopRecognition();
+	}
+}
+ 
