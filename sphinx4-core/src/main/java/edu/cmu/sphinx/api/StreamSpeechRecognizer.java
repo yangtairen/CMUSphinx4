2106352ed6f69c1aeca38f19ce475afa74d87174
giach <georgiana.chelu93@gmail.com>
2014-09-06 10:58:21 +0300

    collect stats for each chunk

--- sphinx4-core/src/main/java/edu/cmu/sphinx/api/StreamSpeechRecognizer.java
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/api/StreamSpeechRecognizer.java
@@ -13,7 +13,13 @@ package edu.cmu.sphinx.api;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
 
+import edu.cmu.sphinx.linguist.acoustic.tiedstate.Sphinx3Loader;
+import edu.cmu.sphinx.speakerid.Segment;
+import edu.cmu.sphinx.speakerid.SpeakerCluster;
+import edu.cmu.sphinx.speakerid.SpeakerIdentification;
 import edu.cmu.sphinx.util.TimeFrame;
 
 
@@ -48,30 +54,97 @@ public class StreamSpeechRecognizer extends AbstractSpeechRecognizer {
      * @throws Exception 
      * @see         StreamSpeechRecognizer#stopRecognition()
      */
-    public void startRecognition(InputStream stream, boolean useOnlineAdaptation) throws Exception {
-        recognizer.allocate();
-        context.setSpeechSource(stream);
-        
-        String path = "/home/gia/Work/sphinx4/sphinx4-samples/src/main/resources/edu/cmu/sphinx/demo/transcriber/mllr_matrix";
-        
-        this.initAdaptation();
-        
-        if(useOnlineAdaptation) {
-        	this.adaptOnline(path, stream);
-        	context.setSpeechSource(profile.localStream);
-        }
-        else {
-        	this.adaptOffline(path);
-        }
+//    public void startRecognition(InputStream stream, boolean useOnlineAdaptation, boolean ok) throws Exception {
+//        recognizer.allocate();
+//        
+//        
+//        context.setSpeechSource(stream);
+//        
+//        
+//        this.initAdaptation();
+//        
+//        if(useOnlineAdaptation) {
+//        	this.adaptOnline(ok);
+//        }
+//        else {
+//        	this.adaptOffline();
+//        }
+//    }
+    public void startRecognition(URL url, boolean useOnlineAdaptation) throws Exception {
+
+    	  collectStatsFromChuncks(url);
+    	
+//        recognizer.allocate();
+//        context.setSpeechSource(stream);
+//        
+//        this.initAdaptation();
+//        
+//        if(useOnlineAdaptation) {
+//        	this.adaptOnline(false);
+//        }
+//        else {
+//        	this.adaptOffline();
+//        }
+    }
+    
+    
     public void startRecognition(InputStream stream, TimeFrame timeFrame) {
         recognizer.allocate();
         context.setSpeechSource(stream, timeFrame);
     }
 
     
-    public void startRecognition(InputStream stream) throws Exception {
-        recognizer.allocate();
-        context.setSpeechSource(stream);
+//    public void startRecognition(InputStream stream) throws Exception {
+//        recognizer.allocate();
+//        context.setSpeechSource(stream);
+//    }
+    
+    
+    // collect stats for each chunck 
+    public void collectStatsFromChuncks(URL url) throws Exception {
+    	recognizer.allocate();
+    
+		SpeakerIdentification sd = new SpeakerIdentification();
+		ArrayList<SpeakerCluster> speakers = sd.cluster(url.openStream());
+		
+		
+		int size = speakers.size();
+		p = new SpeakerProfile[size];
+
+		ArrayList<Segment> segment = new ArrayList<Segment>();
+
+    	TimeFrame t;
+    	int ok;
+    	
+    	int idx = 0;
+    	for (SpeakerCluster spk : speakers) {
+    		segment = spk.getArrayOfSegments();
+
+    		ok = segment.size();
+    		
+    		for(Segment s : segment) {
+    			
+    		   	long startTime = s.getStartTime();
+            	long endTime   = s.getStartTime() + s.getLength();
+            	t = new TimeFrame(startTime, endTime);
+            	
+                 
+            	 context.setSpeechSource(url.openStream(), t);
+                 p[idx] = new SpeakerProfile(context);
+                 
+                 this.initAdaptation(idx);
+                 
+            	// collect stats
+            	if(ok == 1) {
+            		this.adaptOnline(false, idx);
+            	}
+            	else
+            		this.adaptOnline(true, idx);
+                ok--;
+             
+    		}
+    		idx++;
+    	}
     }
     
 	/**
@@ -84,4 +157,10 @@ public class StreamSpeechRecognizer extends AbstractSpeechRecognizer {
     public void stopRecognition() {
         recognizer.deallocate();
     }
+
+
+	public Sphinx3Loader getLoader() {
+		// TODO Auto-generated method stub
+		return (Sphinx3Loader)context.getLoader();
+	}
 }
